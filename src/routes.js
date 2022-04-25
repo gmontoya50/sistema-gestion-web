8d2101f2ebee59dbd3aef10805025bb0847a7a16
Gabriel Montoya <gmontoya50@gmail.com>
2022-04-24 22:39:24 -0400

    mejora en rutas

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,17 @@ const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
 ]
+function renderSwitch(from) {
+    switch (from) {
+      case "admin":
+        return routes;
+      case "user":
+        return routes;
+      default: return [];  
+    }
+}
+const conditional = renderSwitch(
+  window.localStorage.getItem("role"),
+);
 
-export default routes
+export default conditional;
